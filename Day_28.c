410278ab2df924c36461e3ebd780f112dd4a0dac
Riddhima <riddhimag833@gmail.com>
2026-03-01 21:52:06 +1100

    Add files via upload

---
+++ Day_28.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, value;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    scanf("%d", &n);
+
+    if (n <= 0)
+        return 0;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Make it circular
+    temp->next = head;
+
+    // Traversal
+    struct Node* current = head;
+
+    do {
+        printf("%d ", current->data);
+        current = current->next;
+    } while (current != head);
+
+    return 0;
+}
